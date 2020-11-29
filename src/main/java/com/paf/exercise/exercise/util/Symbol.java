b06187c105a463f9adb4b35a371187201fee0ded
Mohammad Fathizadeh <mohammad.fathizadeh@gmail.com>
2020-11-30 00:19:09 +0100

    added more symbolds

--- src/main/java/com/paf/exercise/exercise/util/Symbol.java
+++ src/main/java/com/paf/exercise/exercise/util/Symbol.java
@@ -1,8 +1,51 @@
 package com.paf.exercise.exercise.util;
 
 public enum Symbol {
+    XAUUSD,
+
     EURUSD,
-    USDJPY,
     EURJPY,
-    XAUUSD;
+    EURAUD,
+    EURCAD,
+    EURCHF,
+    EURDKK,
+    EURNOK,
+    EURSEK,
+    EURSGD,
+    EURGBP,
+    EURNZD,
+
+    USDJPY,
+    USDCAD,
+    USDMXN,
+    USDCHF,
+    USDDKK,
+    USDNOK,
+    USDSEK,
+    USDSGD,
+
+    GBPJPY,
+    GBPUSD,
+    GBPNZD,
+    GBPAUD,
+    GBPCAD,
+
+    NZDUSD,
+    NZDCAD,
+    NZDCHF,
+    NZDJPY,
+
+    CADJPY,
+    CADCHF,
+
+    CHFSGD,
+    CHFJPY,
+
+    AUDCAD,
+    AUDNZD,
+    AUDCHF,
+    AUDJPY,
+    AUDSGD,
+    AUDUSD;
+
 }
